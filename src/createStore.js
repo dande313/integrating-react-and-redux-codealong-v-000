e63f49528e1709af15307da63c13c6651f73962d
Allen Anderson <dande313@gmail.com>
2017-11-07 00:05:15 +0000

    automatically backed up by learn

--- src/createStore.js
+++ src/createStore.js
@@ -1,17 +1,19 @@
+import { render } from './index.js';
+ 
 export default function createStore(reducer) {
   let state;
-
+ 
   function dispatch(action) {
     state = reducer(state, action)
     console.log(`the state is ${state.count}`)
     console.log(`the action is ${action.type}`)
-    // render()
+    render();
   };
-
+ 
   function getState() {
     return state;
   };
-
+ 
   return {
     dispatch,
     getState
